7f2f5cbcd7d13ce236022f9cbbbf696613ccf610
Rothenbaum <irothenbaum@rxrrealty.com>
2024-08-17 22:19:01 -0400

    Working on bio cards

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,22 +1,34 @@
 (function($, window) {
   $(document).ready(function() {
-    setTimeout(matchHeights, 100)
+    matchHeights()
+    $(window).on('resize', matchHeights)
   })
+
+  let matchedHeights = false
 
   const lookup = {}
   function matchHeights() {
-    $('[data-match-heights]').each(function(i, elem) {
-      const $elem = $(elem)
-      const group = $elem.attr('data-match-heights')
-      lookup[group] = lookup[group] || {elems: [], max: 0}
-      lookup[group].elems.push($elem)
-      lookup[group].max = Math.max(lookup[group].max, $elem.outerHeight())
-    })
-
-    Object.keys(lookup).forEach(group => {
-      lookup[group].elems.forEach($elem => {
-        $elem.css('height', lookup[group].max)
+    if (matchedHeights) {
+      $('[data-match-heights]').each(function(i, elem) {
+        $(elem).css('height', '')
       })
-    })
+    }
+    setTimeout(() => {
+      $('[data-match-heights]').each(function(i, elem) {
+        const $elem = $(elem)
+        const group = $elem.attr('data-match-heights')
+        lookup[group] = lookup[group] || {elems: [], max: 0}
+        lookup[group].elems.push($elem)
+        lookup[group].max = Math.max(lookup[group].max, $elem.outerHeight())
+      })
+
+      Object.keys(lookup).forEach(group => {
+        lookup[group].elems.forEach($elem => {
+          $elem.css('height', lookup[group].max)
+        })
+      })
+
+      matchedHeights = true
+    }, 100)
   }
 })(jQuery, window, undefined)
